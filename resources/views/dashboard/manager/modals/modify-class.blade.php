b9bf976e98c7bc5a0b68116be3cca479fca69934
OCE2019 <okekechristian1960@yahoo.com>
2021-05-14 11:30:51 +0100

    Fixing of bug 18...

--- resources/views/dashboard/manager/modals/modify-class.blade.php
+++ resources/views/dashboard/manager/modals/modify-class.blade.php
@@ -219,7 +219,7 @@ $(document).ready(function() {
                     $('#div-courseClass-modal-error').show();
 
                     if (data.responseJSON && data.responseJSON.errors){
-                        $.each(result.errors, function(key, value){
+                        $.each(data.responseJSON.errors, function(key, value){
                             $('#div-courseClass-modal-error').append('<li class="">'+value+'</li>');
                         });
                     }
