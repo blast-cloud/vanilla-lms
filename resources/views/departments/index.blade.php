d417e7d167a4317d7cd2d31ee4d1432b4c79e0df
Cornel <shupel16@gmail.com>
2021-12-01 12:30:21 +0100

    css fix

--- resources/views/departments/index.blade.php
+++ resources/views/departments/index.blade.php
@@ -23,7 +23,7 @@ Departments
                 <div class="pull-left"></div>
                 <div class="pull-right">
                     <a id="btn-new-department" href="#" class="btn btn-xs btn-default btn-new-mdl-department-modal"><i class="zmdi zmdi-home"></i>&nbsp;New Department</a>
-                    <a data-toggle="modal" data-target="#mdl-bulk-department-modal" href="#" class="btn btn-primary-alt btn-xs"><i class="icon wb-reply" aria-hidden="true"></i>Bulk upload</a>
+                    <a data-toggle="modal" data-target="#mdl-bulk-department-modal" href="#" class="btn btn-primary-alt btn-xs"><i class="zmdi zmdi-upload" aria-hidden="true"></i>Bulk upload</a>
                 </div>
                 <div class="clearfix"></div>
             </div>
