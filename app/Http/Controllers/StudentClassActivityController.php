25df3f45d07f59561eb4a26d703eca214ddff856
maitiigor <oluwatosinadesoyesalami@gmail.com>
2022-01-25 16:09:48 +0100

    student class activities minor update

--- app/Http/Controllers/StudentClassActivityController.php
+++ app/Http/Controllers/StudentClassActivityController.php
@@ -27,8 +27,29 @@ class StudentClassActivityController extends Controller
        
     }
     public function store(Request $request){
-        $result = $this->studentClassActivityRepository->create($request->all());
-        return response()->json($result);
+
+        $result = 0 ;
+        $student_activity = null;
+        if($request->downloaded == '1'){
+           $query = $this->studentClassActivityRepository->first(['student_id' => $request->student_id,'class_material_id' => $request->class_material_id, 'course_class_id' => $request->course_class_id, 'downloaded' => true]);
+            if(!empty($query)){
+                $student_activity = $query;
+                $result = 1;
+             }
+        }
+        if ($request->clicked == '1') {
+             $query = $this->studentClassActivityRepository->first(['student_id' => $request->student_id,'class_material_id' => $request->class_material_id, 'course_class_id' => $request->course_class_id,'clicked' => true]);
+             if(!empty($query)){
+                $student_activity = $query;
+                $result = 1;
+             }
+
+        }
+        if($result == 0){
+            $student_activity = $this->studentClassActivityRepository->create($request->all());
+        }
+       
+        return response()->json($student_activity);
     }
     public function update(Request $request){
           
